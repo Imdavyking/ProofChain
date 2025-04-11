86761474aa159cb7c29c17b62f680e0c8d174e9f
imdavyking <yungdynamic53@gmail.com>
2025-04-11 20:29:45 +0100

    chore: updated contract test

--- contracts/test/DatasetMarketplace.test.ts
+++ contracts/test/DatasetMarketplace.test.ts
@@ -21,7 +21,7 @@ describe("DatasetMarketplace", function () {
   it("should allow uploading datasets", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     const dataset = await marketplace.datasets(0);
     expect(dataset.cid).to.equal("cid1");
     expect(dataset.owner).to.equal(user1.address);
@@ -30,7 +30,7 @@ describe("DatasetMarketplace", function () {
   it("should allow updating datasets", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await marketplace
       .connect(user1)
       .updateDataset(0, "newCid", ethers.parseEther("2"));
@@ -42,7 +42,7 @@ describe("DatasetMarketplace", function () {
   it("should fail to update dataset if not owner", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await expect(
       marketplace
         .connect(user2)
@@ -56,7 +56,7 @@ describe("DatasetMarketplace", function () {
   it("should allow purchasing access", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await marketplace
       .connect(user2)
       .purchaseAccess(0, { value: ethers.parseEther("1") });
@@ -67,7 +67,7 @@ describe("DatasetMarketplace", function () {
   it("should revert if underpaying for access", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await expect(
       marketplace
         .connect(user2)
@@ -81,7 +81,7 @@ describe("DatasetMarketplace", function () {
   it("should allow rating a dataset", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await marketplace.connect(user2).rateDataset(0, 4);
     const dataset = await marketplace.datasets(0);
     expect(dataset.starsTotal).to.equal(4);
@@ -91,7 +91,7 @@ describe("DatasetMarketplace", function () {
   it("should revert if user rates more than once", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await marketplace.connect(user2).rateDataset(0, 4);
     await expect(
       marketplace.connect(user2).rateDataset(0, 5)
@@ -104,7 +104,7 @@ describe("DatasetMarketplace", function () {
   it("should increment downloads only if user has access", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await marketplace
       .connect(user2)
       .purchaseAccess(0, { value: ethers.parseEther("1") });
@@ -116,7 +116,7 @@ describe("DatasetMarketplace", function () {
   it("should revert download if user has no access", async function () {
     await marketplace
       .connect(user1)
-      .uploadDataset("cid1", ethers.parseEther("1"));
+      .uploadDataset("cid1", ethers.parseEther("1"), 0, "preview", "title");
     await expect(
       marketplace.connect(user2).recordDownload(0)
     ).to.be.revertedWithCustomError(
